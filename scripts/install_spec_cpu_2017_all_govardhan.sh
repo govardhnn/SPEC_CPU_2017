3ad6fc5ea50c1c9dcbf346aa23b363c90351d714
Govardhan <saigov14@gmail.com>
2023-05-02 11:06:53 +0530

    scripts

---
+++ scripts/install_spec_cpu_2017_all_govardhan.sh
@@ -0,0 +1,22 @@
+
+sudo apt-get install build-essential gcc g++ gfortran
+gcc --version
+g++ --version
+gfortran --version
+mkdir /tmp/iso
+sudo mount -o loop,ro cpu2017-1_0_5.iso /tmp/iso
+cd /tmp/iso
+# replace desired directory below
+mkdir <desired directory>/benchmarks
+./install.sh
+cd -
+sudo umount /tmp/iso
+rmdir /tmp/iso
+cd ~/benchmarks
+# replace your config file with a desired name
+cp config/Example-gcc-linux-x86.cfg config/<your config file>.cfg
+. shrc
+cd config
+runcpu --update
+runcpu --config=<your config file>.cfg --iterations=3 --reportable all
+
